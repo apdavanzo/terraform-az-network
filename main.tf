6f481c5c7bfa80e7720477ec0dac1886e18a962d
apdavanzo <39068927+apdavanzo@users.noreply.github.com>
2018-12-07 16:11:04 -0800

    Update Main.tf

--- main.tf
+++ main.tf
@@ -9,23 +9,23 @@ resource "azurerm_resource_group" "network" {
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "network" {
-  name                = "production-network"
+  name                = "test-network"
   address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.network.location}"
   resource_group_name = "${azurerm_resource_group.network.name}"
 
   subnet {
-    name           = "subnet1"
+    name           = "test-subnet1"
     address_prefix = "10.0.1.0/24"
   }
 
   subnet {
-    name           = "subnet2"
+    name           = "test-subnet2"
     address_prefix = "10.0.2.0/24"
   }
 
   subnet {
-    name           = "subnet3"
+    name           = "test-subnet3"
     address_prefix = "10.0.3.0/24"
   }
 }
